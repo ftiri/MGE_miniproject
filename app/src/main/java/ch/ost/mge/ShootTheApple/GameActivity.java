ae5ad53ad77a2ef01adc708db51560c6a9724717
Bojan Dakic <bojan.dakic@hsr.ch>
2020-10-23 19:10:20 +0200

    game_acitivity added

---
+++ app/src/main/java/ch/ost/mge/ShootTheApple/GameActivity.java
@@ -0,0 +1,6 @@
+package ch.ost.mge.ShootTheApple;
+
+public class GameActivity {
+    private static final long LIFE_TIME = 2000;
+    private static final int[] ENEMY_PICTURES = {R.drawable.apple_iphone_39x54, R.drawable.apple_logo_49x27, R.drawable.apple_logo_rainbow_43x52, R.drawable.apple_macbook_56x39, R.drawable.android_48x56};
+}
