cfeb94df84480f99978c691fde5b10533830338b
Bojan Dakic <bojan.dakic@hsr.ch>
2020-10-24 15:29:46 +0200

    game_acitivity started working / pause because of soccer

---
+++ app/src/main/java/ch/ost/mge/ShootTheApple/CameraView.java
@@ -0,0 +1,65 @@
+package ch.ost.mge.ShootTheApple;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+
+public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder surfaceHolder;
+    private Camera camera;
+
+    public CameraView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public CameraView(Context context) {
+        super(context);
+        surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        camera = Camera.open();
+        try {
+            camera.setPreviewDisplay(holder);
+        } catch (IOException e) {
+            camera.release();
+            camera = null;
+        }
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int w, int h, int i2) {
+        try {
+            Camera.Parameters parameters = camera.getParameters();
+            parameters.setPreviewSize(w, h);
+            camera.setParameters(parameters);
+        } catch(Exception e) {
+            Log.w("CameraView", "Exception:", e);
+        }
+        camera.startPreview();
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    }
+
+    public void setOneShotPreviewCallback(Camera.PreviewCallback callback) {
+        if(camera != null) {
+            camera.setOneShotPreviewCallback(callback);
+        }
+    }
+}
--- app/src/main/java/ch/ost/mge/ShootTheApple/GameActivity.java
+++ app/src/main/java/ch/ost/mge/ShootTheApple/GameActivity.java
@@ -1,6 +1,101 @@
 package ch.ost.mge.ShootTheApple;
 
-public class GameActivity {
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.TextureView;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class GameActivity extends AppCompatActivity implements Camera.PreviewCallback, Runnable {
     private static final long LIFE_TIME = 2000;
-    private static final int[] ENEMY_PICTURES = {R.drawable.apple_iphone_39x54, R.drawable.apple_logo_49x27, R.drawable.apple_logo_rainbow_43x52, R.drawable.apple_macbook_56x39, R.drawable.android_48x56};
+    private static final int[] PICTURES = {R.drawable.apple_iphone_39x54, R.drawable.apple_logo_49x27, R.drawable.apple_logo_rainbow_43x52, R.drawable.apple_macbook_56x39, R.drawable.android_48x56};
+    private static final int[][] SIZES = {
+            {39, 54},
+            {49, 27},
+            {43, 52},
+            {56, 39},
+            {48, 56}
+    };
+    private int points;
+    private int lifes;
+    private Random generator = new Random();
+    private CameraView camera;
+    private ViewGroup playMat;
+    private float density;
+    private Handler handler = new Handler();
+    private TextView pointsView = (TextView) findViewById(R.id.points);
+    private TextView lifeView = (TextView) findViewById(R.id.lifes);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        camera = (CameraView) findViewById(R.id.camera);
+        playMat = (ViewGroup) findViewById(R.id.play_mat);
+        density =getResources().getDisplayMetrics().density;
+        points = 0;
+        lifes = 3;
+        camera.setOneShotPreviewCallback(this);
+        playGame();
+    }
+
+    private void playGame() {
+        refreshScreen();
+        handler.postDelayed(this, 1000);
+    }
+
+    private void refreshScreen() {
+        pointsView.setText(R.string.points + ": " + Integer.toString(points));
+        lifeView.setText(R.string.life + ": " + Integer.toString(lifes));
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] bytes, Camera camera) {
+
+    }
+
+    @Override
+    public void run() {
+        float randomNumber = generator.nextFloat();
+        int multiplier = points / 20000 + 1;
+        for(int i = 0; i < randomNumber * multiplier + 1; i++) {
+            spawn(generator.nextFloat());
+        }
+    }
+
+    private void spawn(float number) {
+        int index = generateIndex(number);
+        int width = playMat.getWidth();
+        int height = playMat.getHeight();
+
+        int symbolWidth = Math.round(density * SIZES[index][0]);
+        int symbolHeight = Math.round(density * SIZES[index][1]);
+
+        int left = generator.nextInt(width - symbolWidth);
+        int top = generator.nextInt(height - symbolHeight);
+
+        ImageView symbol = new ImageView(this);
+    }
+
+    private int generateIndex(float number) {
+        if(number < 0.2) {
+            return 0;
+        }
+        if(number < 0.4) {
+            return 1;
+        }
+        if(number < 0.6) {
+            return 2;
+        }
+        if(number < 0.8) {
+            return 3;
+        }
+        return 4;
+    }
 }
