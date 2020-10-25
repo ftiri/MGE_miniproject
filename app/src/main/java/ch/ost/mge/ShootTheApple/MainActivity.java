82551e47c77d2a9a61b6ee6703cd159121898814
Fabian Tiri <fabiantiri@bluewin.ch>
2020-10-25 16:25:19 +0100

    add theme styles

--- app/src/main/java/ch/ost/mge/ShootTheApple/MainActivity.java
+++ app/src/main/java/ch/ost/mge/ShootTheApple/MainActivity.java
@@ -9,16 +9,29 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     private Button startGame;
+    private Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startGame = (Button) findViewById(R.id.start_button);
         startGame.setOnClickListener(this);
+
+        spinner = (Spinner)findViewById(R.id.selectionThemeButton);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.themeSelection, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             startGame.setEnabled(false);
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 0);
@@ -36,5 +49,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(requestCode == 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             startGame.setEnabled(true);
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
+
+        switch(pos){
+            case 0:
+                setTheme(R.style.OstTheme);
+                break;
+            case 1:
+                setTheme(R.style.HsrTheme);
+                break;
+            case 2:
+                setTheme(R.style.DarkMode);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
     }
 }
