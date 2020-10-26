099ee7f72186e340e95d4c21c0dc9e0a956f23c2
Bojan Dakic <bojan.dakic@hsr.ch>
2020-10-26 19:57:36 +0100

    ranking done in previous but message forgot / added remaining languages

--- app/src/main/java/ch/ost/mge/ShootTheApple/RankingActivity.java
+++ app/src/main/java/ch/ost/mge/ShootTheApple/RankingActivity.java
@@ -1,10 +1,7 @@
 package ch.ost.mge.ShootTheApple;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.InputFilter;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
